aebd64884c24f9b723492559f4113139afdc01c8
Cam Tullos <cam@reactium.io>
2022-10-06 16:37:03 -1000

    Updated for Actinium 4.0.0 usage. Published @atomic-reactor/actinium-auth@1.1.4.

--- actinium/middleware.js
+++ actinium/middleware.js
@@ -2,19 +2,23 @@ const path = require("path");
 const fs = require("fs-extra");
 const express = require("express");
 
-Actinium.Middleware.register(
-  "auth",
-  app => {
-    const router = express.Router();
-    const STATIC_PATH = path.normalize(`${__dirname}/public`);
+const MW = Actinium => {
+  Actinium.Middleware.register(
+    "auth",
+    app => {
+      const router = express.Router();
+      const STATIC_PATH = path.normalize(`${__dirname}/public`);
 
-    fs.ensureDirSync(STATIC_PATH);
+      fs.ensureDirSync(STATIC_PATH);
 
-    router.use("/api/auth", express.static(STATIC_PATH));
+      router.use("/api/auth", express.static(STATIC_PATH));
 
-    app.use(router);
+      app.use(router);
 
-    return Promise.resolve();
-  },
-  -1000
-);
+      return Promise.resolve();
+    },
+    -1000
+  );
+}
+
+module.exports = Actinium ? MW(Actinium) : MW;
